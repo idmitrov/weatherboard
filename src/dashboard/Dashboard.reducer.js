da67c116f2275687648b709064a1d30de9f24328
Dimitar <mailto.dimitrov@gmail.com>
2019-11-24 10:42:25 +0200

    Add dashboard reducer

---
+++ src/dashboard/Dashboard.reducer.js
@@ -0,0 +1,11 @@
+const dashboardDefaults = {
+  fiveDaysForecast: null
+};
+
+const dashboardReducer = (state = dashboardDefaults, action = {}) => {
+  switch (action.type) {
+    default: return state;
+  }
+};
+
+export default dashboardReducer;
