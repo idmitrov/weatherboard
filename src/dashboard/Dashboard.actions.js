dbf0c77792c73a4a8c0b117d24d232a5815595fc
Dimitar <mailto.dimitrov@gmail.com>
2019-11-26 01:13:25 +0200

    Add doc

--- src/dashboard/Dashboard.actions.js
+++ src/dashboard/Dashboard.actions.js
@@ -5,6 +5,11 @@ export const dashboardActionTypes = {
   setForecastForFiveDays: 'FORECAST_FIVE_DAYS_SET'
 };
 
+/**
+ * Dispatch API action which API middleware to handle
+ * and do API request
+ * @name fetchForecastForFiveDays
+ */
 export const fetchForecastForFiveDays = () => {
   const action = {
     type: dashboardActionTypes.fetchForecastForFiveDays,
@@ -17,6 +22,11 @@ export const fetchForecastForFiveDays = () => {
   return action;
 };
 
+/**
+ * Dispatch action to set the recived forecast API data
+ * @name fetchForecastForFiveDays
+ * @param {Object} data
+ */
 export const setForecastForFiveDays = (data) => {
   const { list, city } = data;
 
