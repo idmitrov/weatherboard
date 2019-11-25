a1547459810878a1c1b6cab1796a47006adfe229
Dimitar <mailto.dimitrov@gmail.com>
2019-11-25 14:23:45 +0200

    Implement WeatherWindMeter component and use it on Dashboard

---
+++ src/components/weather-wind-meter/WeatherWindMeter.component.js
@@ -0,0 +1,79 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+
+import {
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Tooltip
+} from 'recharts';
+
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(() => {
+  return {
+    tooltipWrapper: {
+      padding: 5,
+      backgroundColor: 'rgba(255, 255, 255, .8)',
+      boxShadow: '0 0 2px rgba(0, 0, 0, .4)'
+    }
+  };
+});
+
+const WeatherWindMeter = ({ data }) => {
+  const { t } = useTranslation();
+  const classes = useStyles();
+
+  return (
+    <Fragment>
+      <Typography variant="h5" align="center">{t('forecast.windSpeed')}</Typography>
+
+      <ResponsiveContainer width="100%" aspect={4.0 / 3.0}>
+        <PieChart>
+          <Pie
+            dataKey="speed"
+            startAngle={180}
+            endAngle={0}
+            data={data}
+            outerRadius="80%"
+            label
+            fill="#8884d8"
+          />
+
+          <Tooltip content={({ payload }) => {
+            if (payload && payload[0]) {
+              const data = payload[0];
+
+              return (
+                <div className={classes.tooltipWrapper}>
+                  <Typography variant="h6">
+                    {t(`week.dayName.${data.name}`)}
+                  </Typography>
+
+                  <Typography>
+                    {`${t('forecast.windSpeed')} ${data.payload.speed} m/s`}
+                  </Typography>
+                </div>
+              );
+            }
+
+            return null;
+          }} />
+        </PieChart>
+      </ResponsiveContainer>
+    </Fragment>
+  );
+};
+
+WeatherWindMeter.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      deg: PropTypes.number,
+      speed: PropTypes.number,
+    })
+  )
+};
+
+export default WeatherWindMeter;
--- src/dashboard/Dashboard.view.js
+++ src/dashboard/Dashboard.view.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Grid } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 
 import Carousel from "../components/carousel/Carousel.component";
 import { fetchForecastForFiveDays, setForecastForFiveDays } from './Dashboard.actions';
 import WeatherDayCard from '../components/weather-day-card/WeatherDayCard.component';
 import WeatherCityBar from '../components/weather-city-bar/WeatherCityBar.component';
+import WeatherWindMeter from '../components/weather-wind-meter/WeatherWindMeter.component';
+import { Grid } from '@material-ui/core';
 
 const filterUniqueDays = (days) => {
   const uniqueDays = days.reduce((daysMerge, day) => {
@@ -30,6 +31,8 @@ const Dashboard = () => {
   const { list, city } = useSelector((state) => state.dashboard.fiveDaysForecast);
 
   const uniqueDays = filterUniqueDays(list);
+  const uniqueDaysWind = uniqueDays.map((day) => day.wind)
+    .sort((a, b) => a.speed > b.speed ? 1 : a.speed < b.speed ? -1 : 0);
 
   useEffect(() => {
     const fetchForecastData = async () => {
@@ -45,43 +48,50 @@ const Dashboard = () => {
     <div>
       {
         city && city.id ? (
-          <WeatherCityBar
-            name={city.name}
-            country={city.country}
-            sunrise={new Date(city.sunrise * 1000)}
-            sunset={new Date(city.sunset * 1000)}
-          />
+          <Grid container>
+            <Grid item xs={12}>
+              <WeatherCityBar
+                name={city.name}
+                country={city.country}
+                sunrise={new Date(city.sunrise * 1000)}
+                sunset={new Date(city.sunset * 1000)}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+
+              <Carousel slidesToShow={4} slidesToScroll={4}>
+                {
+                  uniqueDays.map((currentDay, index) => {
+                    const currentDayWeather = currentDay.weather[0];
+                    const currentDayDate = new Date(currentDay.dt * 1000);
+
+                    const date = t('date.ddmmyy', { date: currentDayDate.getDate(), month: currentDayDate.getMonth(), year: currentDayDate.getFullYear() });
+                    const day = t(`week.dayName.${currentDayDate.getDay()}`);
+                    const description = t(`forecast.description.${currentDayWeather.main.toLowerCase()}`);
+                    const imageUrl = `${process.env.PUBLIC_URL}/weather-icons/${currentDayWeather.icon}.png`;
+                    const temperature = Math.round(currentDay.main.temp);
+
+                    return (
+                      <WeatherDayCard
+                        key={index}
+                        temperature={temperature}
+                        date={date}
+                        dayName={day}
+                        description={description}
+                        imageUrl={imageUrl}
+                      />
+                    );
+                  })
+                }
+              </Carousel>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <WeatherWindMeter data={uniqueDaysWind} />
+            </Grid>
+          </Grid>
         ) : (null)}
-
-      {
-        list && list.length ? (
-          <Carousel slidesToShow={4} slidesToScroll={4}>
-            {
-              uniqueDays.map((currentDay, index) => {
-                const currentDayWeather = currentDay.weather[0];
-                const currentDayDate = new Date(currentDay.dt * 1000);
-
-                const date = t('date.ddmmyy', { date: currentDayDate.getDate(), month: currentDayDate.getMonth(), year: currentDayDate.getFullYear() });
-                const day = t(`week.dayName.${currentDayDate.getDay()}`);
-                const description = t(`forecast.description.${currentDayWeather.main.toLowerCase()}`);
-                const imageUrl = `${process.env.PUBLIC_URL}/weather-icons/${currentDayWeather.icon}.png`;
-                const temperature = Math.round(currentDay.main.temp);
-
-                return (
-                  <WeatherDayCard
-                    key={index}
-                    temperature={temperature}
-                    date={date}
-                    dayName={day}
-                    description={description}
-                    imageUrl={imageUrl}
-                  />
-                );
-              })
-            }
-          </Carousel>
-        ) : (null)
-      }
     </div>
   );
 };
